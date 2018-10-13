f43efba4ba092fc83aaf5c74c51830a737f0cf12
nanyash <miss.nanya80@gmail.com>
2018-10-13 23:11:22 +0300

    initial

--- src/main/java/com/dao/imp/AdvertDAOImp.java
+++ src/main/java/com/dao/imp/AdvertDAOImp.java
@@ -48,7 +48,6 @@ public class AdvertDAOImp extends GenericDAO<Advert> implements AdvertDAO{
         Root<Advert> root = criteria.from(Advert.class);
         Join<Advert, Property> join = root.join("property");
 
-
         Predicate predicate = builder.conjunction();
 
         Set<String> params = filterParms.entrySet().stream()
@@ -59,18 +58,19 @@ public class AdvertDAOImp extends GenericDAO<Advert> implements AdvertDAO{
                 .collect(Collectors.toSet());
 
         for (String param : params) {
-
             if (param.equals("description")) {
                 predicate = builder.and(predicate, builder.like(root.get(param), filterParms.get(param).toString()));
                 continue;
             }
             predicate = builder.and(predicate, builder.equal(join.get(param), filterParms.get(param)));
         }
+
         if (filter.getPropertyType() != null)
             predicate = builder.and(predicate, builder.equal(join.get("propertyType"),filter.getPropertyType()));
 
         if (predicate != null)
             return getEntityManager().createQuery(criteria.select(root).where(predicate)).getResultList();
+
         return getEntityManager().createQuery(criteria.select(root)).getResultList();
     }
 }
